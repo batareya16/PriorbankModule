9c7297b91f63cb8967ab052844723e4988a5c979
Batareya16 <kitten2@list.ru>
2019-02-21 00:13:51 +0300

    Added PriorbankLockedTransaction class

---
+++ PriorbankModule/Entities/PriorbankLockedTransaction.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PriorbankModule.Entities
+{
+    class PriorbankLockedTransaction
+    {
+        public DateTime ATransDate { get; set; }
+
+        public string ATransDetails { get; set; }
+
+        public double ATransAmountString { get; set; }
+
+        public double AAmountString { get; set; }
+
+        public long AAmount { get; set; }
+
+        public long ATransAmount { get; set; }
+
+        public string ATransCurr { get; set; }
+
+        public DateTime ATransTime { get; set; }
+
+        public string ContractCurr { get; set; }
+
+        public bool IsHce { get; set; }
+    }
+}
